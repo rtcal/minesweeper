031e7c328171e960a746b09b6c16923883ed93f0
= <c.r.turasz@gmail.com>
2021-12-10 22:46:45 +0000

    base game implementation

--- src/main/java/com/rtcal/minesweeper/game/Game.java
+++ src/main/java/com/rtcal/minesweeper/game/Game.java
@@ -18,26 +18,87 @@ public final class Game {
     }
 
     public Tile getTile(Location location) {
-        return null;
+        int x = location.getBlockX() - boardLocation.getBlockX();
+        int z = location.getBlockZ() - boardLocation.getBlockZ();
+
+        return location.getBlockY() == boardLocation.getBlockY() ? grid.getTile(x, z) : null;
     }
 
     public void flagTile(Tile tile) {
+        tile.toggle();
 
+        Location loc = boardLocation.clone().add(tile.getX(), 0, tile.getZ());
+        Material mat = tile.isFlagged() ? TileMaterial.getFlagged() : TileMaterial.getCover();
+
+        loc.getBlock().setType(mat);
     }
 
-    public void revealTile(Tile tile) {
+    private void reveal(Tile tile) {
+        tile.reveal();
+        grid.decrementRemaining();
+    }
 
+    public boolean revealTile(Tile tile) {
+        if (tile.isRevealed() || tile.isFlagged()) return false;
+
+        if (tile.isBomb()) {
+            revealBoard();
+            return true;
+        }
+
+        reveal(tile);
+
+        Location loc = boardLocation.clone().add(tile.getX(), -1, tile.getZ());
+        loc.getBlock().setType(TileMaterial.getTile(tile.getValue()));
+        loc.add(0, 1, 0).getBlock().setType(Material.AIR);
+
+        if (tile.getValue() == 0) floodReveal(tile);
+
+        return false;
+    }
+
+    private void floodReveal(Tile tile) {
+        for (Tile neighbour : grid.getNeighbours(tile)) {
+            if (neighbour.isRevealed() || neighbour.isFlagged() || neighbour.isBomb() || tile.getValue() > 0) continue;
+            revealTile(neighbour);
+        }
     }
 
     public void revealBoard() {
+        for (int x = 0; x < width; x++) {
 
+            for (int z = 0; z < length; z++) {
+                Tile tile = grid.getTile(x, z);
+
+                if (tile == null) continue;
+
+                Location loc = boardLocation.clone();
+
+                loc.add(x, -1, z).getBlock().setType(getTileMaterial(tile));
+                loc.add(0, 1, 0).getBlock().setType(Material.AIR);
+
+            }
+        }
     }
 
     public void spawnBoard() {
+        for (int x = 0; x < width; x++) {
 
+            for (int z = 0; z < length; z++) {
+                Tile tile = grid.getTile(x, z);
+
+                if (tile == null) continue;
+
+                Location loc = boardLocation.clone();
+
+                loc.add(x, 0, z).getBlock().setType(TileMaterial.getCover());
+                loc.subtract(0, 1, 0).getBlock().setType(Material.AIR);
+
+            }
+        }
     }
 
-    public Material getTileMaterial(Tile tile) {
+    private Material getTileMaterial(Tile tile) {
         return tile.isBomb() ? TileMaterial.getBomb() : TileMaterial.getTile(tile.getValue());
     }
 }
