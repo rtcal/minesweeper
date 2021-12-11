ea459100e8b36fd1b28dc0931cff9335d0e70378
= <c.r.turasz@gmail.com>
2021-12-11 22:51:27 +0000

    Colour code support

--- src/main/java/com/rtcal/minesweeper/Messages.java
+++ src/main/java/com/rtcal/minesweeper/Messages.java
@@ -1,24 +1,25 @@
 package com.rtcal.minesweeper;
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public final class Messages {
 
     private static final FileConfiguration config = Main.getInstance().getConfig();
 
-    private static final String PREFIX = config.getString("prefix") + " ";
+    private static final String PREFIX = ChatColor.translateAlternateColorCodes('&', config.getString("prefix") + " ");
 
-    public static final String BROADCAST_LEAVE = PREFIX + config.getString("messages.broadcast_leave");
-    public static final String BROADCAST_JOIN = PREFIX + config.getString("messages.broadcast_join");
-    public static final String BROADCAST_START = PREFIX + config.get("messages.broadcast_start");
-    public static final String BROADCAST_OVER = PREFIX + config.get("messages.broadcast_over");
+    public static final String BROADCAST_LEAVE = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.broadcast_leave"));
+    public static final String BROADCAST_JOIN = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.broadcast_join"));
+    public static final String BROADCAST_START = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.broadcast_start"));
+    public static final String BROADCAST_OVER = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.broadcast_over"));
 
-    public static final String PLAYER_BUSY = PREFIX + config.getString("messages.player_busy");
-    public static final String PLAYER_ONLY_COMMAND = PREFIX + config.getString("messages.player_only_command");
+    public static final String PLAYER_BUSY = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.player_busy"));
+    public static final String PLAYER_ONLY_COMMAND = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.player_only_command"));
 
-    public static final String ARENA_FULL = PREFIX + config.getString("messages.arena_full");
-    public static final String ARENA_WRONG_WORLD = PREFIX + config.getString("messages.arena_wrong_world");
+    public static final String ARENA_FULL = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.arena_full"));
+    public static final String ARENA_WRONG_WORLD = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.arena_wrong_world"));
 
-    public static final String ARENA_CREATION_SUCCESS = PREFIX + config.getString("messages.arena_creation_success");
-    public static final String ARENA_CREATION_FAILED = PREFIX + config.getString("messages.arena_creation_failed");
+    public static final String ARENA_CREATION_SUCCESS = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.arena_creation_success"));
+    public static final String ARENA_CREATION_FAILED = PREFIX + ChatColor.translateAlternateColorCodes('&', config.getString("messages.arena_creation_failed"));
 }
